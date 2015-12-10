198c415591df1a08e206ebb746c3c2fd62c58b75
Andrea Luzzati <andrea.luzzati@gmail.com>
2015-12-10 18:51:34 +0100

    About.java: added github reference

--- src/commands/About.java
+++ src/commands/About.java
@@ -7,7 +7,7 @@ import telegram.User;
 public class About implements telegram.Command
 {
    // Id of a preloaded Sticker
-   private String stickerID = "BQADBAADWwADDOJqCFSFO2wy98auAg";
+   private String stickerID = "BQADBAADAgADy8SJCDzXkd29_9R2Ag";
    @Override
    public void execute(User caller, Chat chat, String[] args, TeleBot botRef) 
    {
@@ -17,7 +17,8 @@ public class About implements telegram.Command
             "I was born because of a very tiny opening.. " +
             "in between my Creator's duties.\n" +
             "Enjoy fag math formulas.\n" +            
-            "p.s.: I speak Java ☕.\n" +
+            "p.s.: I speak Java ☕, find me at https://github.com/AndreaLu/TeXBoT.\n" +
+            "Talk to me: @alu_texbot\n" +
             "\n" +
             "TeXBoT v0.2β\n" +
             "Release date: 7 December 2015\n" +
