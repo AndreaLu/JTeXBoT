7177f8172b3102fc8f5889a662162f4be33395e8
Andrea Luzzati <andrea.luzzati@gmail.com>
2016-05-11 23:44:29 +0200

    removed useless file

--- src/GenericTODOS.java
+++
@@ -1,7 +0,0 @@
-
-public class GenericTODOS 
-{
-	// TODO handle all types of update (write down TeleBot.sendUpdates body)
-	// TODO change updates file structure: different updates should be embedded in a unique JSON
-	// structure instead of separated by encapsulation {update0}{update1}...
-}
